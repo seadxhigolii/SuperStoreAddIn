ddb3fbd6725fd03ae9727194bc71c7d5d36c886f
Sead Xhigoli <sead.xhigoli1@gmail.com>
2025-03-29 01:19:41 +0100

    Add project files.

---
+++ SuperStoreAddIn/SuperstoreRibbon.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SuperStoreAddIn
+{
+    partial class SuperstoreRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public SuperstoreRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnHighlightHighSales = this.Factory.CreateRibbonButton();
+            this.btnShowSummary = this.Factory.CreateRibbonButton();
+            this.btnClearHighlights = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnHighlightHighSales);
+            this.group1.Items.Add(this.btnShowSummary);
+            this.group1.Items.Add(this.btnClearHighlights);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // btnHighlightHighSales
+            // 
+            this.btnHighlightHighSales.Label = "Highlight High Sales";
+            this.btnHighlightHighSales.Name = "btnHighlightHighSales";
+            this.btnHighlightHighSales.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnHighlightHighSales_Click);
+            // 
+            // btnShowSummary
+            // 
+            this.btnShowSummary.Label = "Show Summary";
+            this.btnShowSummary.Name = "btnShowSummary";
+            this.btnShowSummary.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnShowSummary_Click);
+            // 
+            // btnClearHighlights
+            // 
+            this.btnClearHighlights.Label = "Clear Highlights";
+            this.btnClearHighlights.Name = "btnClearHighlights";
+            this.btnClearHighlights.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnClearHighlights_Click);
+            // 
+            // SuperstoreRibbon
+            // 
+            this.Name = "SuperstoreRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.SuperstoreRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnHighlightHighSales;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnShowSummary;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnClearHighlights;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal SuperstoreRibbon SuperstoreRibbon
+        {
+            get { return this.GetRibbon<SuperstoreRibbon>(); }
+        }
+    }
+}
---
+++ SuperStoreAddIn/SuperstoreRibbon.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using oi = Microsoft.Office.Interop;
+
+namespace SuperStoreAddIn
+{
+    public partial class SuperstoreRibbon
+    {
+        private void SuperstoreRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnHighlightHighSales_Click(object sender, RibbonControlEventArgs e)
+        {
+            oi.Excel.Worksheet sheet = Globals.ThisAddIn.Application.ActiveSheet;
+            oi.Excel.Range usedRange = sheet.UsedRange;
+
+            int salesColumn = 0;
+            for (int i = 1; i <= usedRange.Columns.Count; i++)
+            {
+                if ((usedRange.Cells[1, i] as oi.Excel.Range).Value2.ToString() == "Sales")
+                {
+                    salesColumn = i;
+                    break;
+                }
+            }
+
+            if (salesColumn == 0) return;
+
+            for (int i = 2; i <= usedRange.Rows.Count; i++)
+            {
+                var cell = usedRange.Cells[i, salesColumn] as oi.Excel.Range;
+                double value = Convert.ToDouble(cell.Value2);
+                if (value > 500)
+                {
+                    (cell.EntireRow).Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.LightGreen);
+                }
+            }
+        }
+
+        private void btnShowSummary_Click(object sender, RibbonControlEventArgs e)
+        {
+            oi.Excel.Worksheet sheet = Globals.ThisAddIn.Application.ActiveSheet;
+            oi.Excel.Range usedRange = sheet.UsedRange;
+
+            double totalSales = 0;
+            int salesColumn = 0;
+
+            for (int i = 1; i <= usedRange.Columns.Count; i++)
+            {
+                if ((usedRange.Cells[1, i] as oi.Excel.Range).Value2.ToString() == "Sales")
+                {
+                    salesColumn = i;
+                    break;
+                }
+            }
+
+            if (salesColumn == 0) return;
+
+            for (int i = 2; i <= usedRange.Rows.Count; i++)
+            {
+                var cell = usedRange.Cells[i, salesColumn] as oi.Excel.Range;
+                totalSales += Convert.ToDouble(cell.Value2);
+            }
+
+            int orderCount = usedRange.Rows.Count - 1;
+            MessageBox.Show($"Total Sales: ${totalSales:N2}\nTotal Orders: {orderCount}", "Sales Summary");
+        }
+
+        private void btnClearHighlights_Click(object sender, RibbonControlEventArgs e)
+        {
+            oi.Excel.Worksheet sheet = Globals.ThisAddIn.Application.ActiveSheet;
+            oi.Excel.Range usedRange = sheet.UsedRange;
+            usedRange.Interior.ColorIndex = oi.Excel.XlColorIndex.xlColorIndexNone;
+        }
+    }
+}
